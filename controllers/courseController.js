597eb8e70dc28e6a352c4692a7a69632c4489abe
aghabalaguluzade <agabala.oyunda@gmail.com>
2022-11-09 02:22:31 +0400

    e-course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -104,7 +104,7 @@ const releaseCourse = async (req,res) => {
 
 const deleteCourse = async (req,res) => {
      try {
-          const course = await Course.findByIdAndRemove({ slug : req.params.slug });
+          const course = await Course.findOneAndRemove({ slug : req.params.slug });
           req.flash("success", `${course.name} has been removed successfully`);
           res.status(200).redirect("/users/dashboard");
      } catch (error) {
